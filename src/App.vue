afee67877dbb7b46109d3fb2eda11a6a615a5f81
Z-WICK <2289431216@qq.com>
2024-12-09 17:07:28 +0800

    init: 项目初始化

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <BasicLayout />
+  </div>
+</template>
+<script setup lang="ts">
+import BasicLayout from '@/layouts/BasicLayout.vue'
+import { useLoginUserStore } from '@/stores/useLoginUserStore.ts'
+import { healthUsingGet } from '@/api/mainController.ts'
+
+const loginUserStore = useLoginUserStore()
+loginUserStore.fetchLoginUser()
+
+healthUsingGet().then((res) => {
+  console.log(res)
+})
+
+</script>
+
+<style>
+#app {
+}
+</style>
